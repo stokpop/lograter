afdddf2304da0e15620142d12e78d223aa6364dd
Naser Ayat <naser.ayat@rabobank.nl>
2019-11-21 16:05:28 +0100

    add tests

---
+++ src/test/java/nl/stokpop/lograter/gc/jmx/parse/CsvFileParserTest.java
@@ -0,0 +1,62 @@
+package nl.stokpop.lograter.gc.jmx.parse;
+
+import nl.stokpop.lograter.gc.jmx.GcMetrics;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class CsvFileParserTest {
+    @Test
+    public void parseConcurrentMarkSweepTest() {
+        List<GcMetrics> memoryMetrics = CsvFileParser.INSTANCE
+                .parse(getFile("jmx/jvm-heap-metrics-concurrent-mark-sweep.log")).collect(Collectors.toList());
+
+        assertEquals(5, memoryMetrics.size());
+        assertEquals(14266424, memoryMetrics.get(4).getHeapMemoryUsedBytes());
+        assertEquals(562416, memoryMetrics.get(4).getOldGenerationUsedBytes());
+        assertEquals(0, memoryMetrics.get(4).getYoungGenerationGcTime());
+        assertEquals(120, memoryMetrics.get(4).getOldGenerationGcTime());
+        assertEquals(120, (long) memoryMetrics.get(4).getGcDurationMs());
+        assertEquals(562416, memoryMetrics.get(4).getOldGenerationUsedBytes());
+    }
+
+    @Test
+    public void detectG1Test() {
+        List<GcMetrics> memoryMetrics = CsvFileParser.INSTANCE
+                .parse(getFile("jmx/jvm-heap-metrics-G1.log")).collect(Collectors.toList());
+
+        assertEquals(4, memoryMetrics.size());
+        assertEquals(13816320, memoryMetrics.get(3).getHeapMemoryUsedBytes());
+        assertEquals(3330560, memoryMetrics.get(3).getOldGenerationUsedBytes());
+        assertEquals(100, memoryMetrics.get(3).getYoungGenerationGcTime());
+        assertEquals(500, memoryMetrics.get(3).getOldGenerationGcTime());
+        assertEquals(600, (long) memoryMetrics.get(3).getGcDurationMs());
+        assertEquals(3330560, memoryMetrics.get(3).getOldGenerationUsedBytes());
+    }
+
+    @Test
+    public void detectMarkSweepCompactTest() {
+        List<GcMetrics> memoryMetrics = CsvFileParser.INSTANCE
+                .parse(getFile("jmx/jvm-heap-metrics-mark-sweep-compact.log")).collect(Collectors.toList());
+
+        assertEquals(10, memoryMetrics.size());
+    }
+
+    @Test
+    public void detectParallelTest() {
+        List<GcMetrics> memoryMetrics = CsvFileParser.INSTANCE
+                .parse(getFile("jmx/jvm-heap-metrics-parallel.log")).collect(Collectors.toList());
+
+        assertEquals(11, memoryMetrics.size());
+    }
+
+    private File getFile(String filePath) {
+        URL resource = this.getClass().getClassLoader().getResource(filePath);
+        return new File(resource.getFile());
+    }
+}
---
+++ src/test/java/nl/stokpop/lograter/gc/jmx/parse/JmxMemoryParserTest.java
@@ -0,0 +1,38 @@
+package nl.stokpop.lograter.gc.jmx.parse;
+
+import nl.stokpop.lograter.gc.jmx.GcMetrics;
+import nl.stokpop.lograter.util.time.TimePeriod;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class JmxMemoryParserTest {
+
+    @Test
+    public void getMemoryMetricsEntriesFromFileTes() {
+        TimePeriod timePeriod = TimePeriod.createExcludingEndTime(1572875922000L, 1572875950000L);
+        List<GcMetrics> memoryMetrics = JmxMemoryParser.INSTANCE.getMemoryMetricsEntriesFromFile(getFile("jmx/jvm-heap-metrics-mark-sweep-compact.log"), timePeriod);
+
+        assertEquals(9, memoryMetrics.size());
+    }
+
+    @Test
+    public void getMemoryMetricsEntriesFromFilesTes() {
+        TimePeriod timePeriod = TimePeriod.MAX_TIME_PERIOD;
+        List<File> files = Arrays.asList(getFile("jmx/jvm-heap-metrics-mark-sweep-compact.log"), getFile("jmx/jvm-heap-metrics-G1.log"));
+
+        List<GcMetrics> memoryMetrics = JmxMemoryParser.INSTANCE.getMemoryMetricsEntriesFromFiles(files, timePeriod);
+
+        assertEquals(14, memoryMetrics.size());
+    }
+
+    private File getFile(String filePath) {
+        URL resource = this.getClass().getClassLoader().getResource(filePath);
+        return new File(resource.getFile());
+    }
+}
